d69229ed7749879871c2914f57f4f8f00e274de5
Dimasbriliant29 <dimasbriliant29@gmail.com>
2023-03-26 01:18:03 +0700

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,88 @@
+<html>
+
+<head>
+    <link rel="icon" href="https://fancodelive.com/briliant.png" type="image/x-icon">
+   <title>Hacked By./BRILIANT</title>
+   <meta name="title" content="HACKED BY ./BRILIANT">
+   <meta name="description" content="HACKED BY ./BRILIANT, BRILIANT, ./BRILIANT, ./DIMAS_BRILIANT, HACKED BY, HACKED BY BRILIANT, HACKED BY ./BRILIANT, BRILIANT, briliant, hacked by ./briliant, hacked by briliant, HACKED BY BRILIANT, briliant29, BRILIANT29, ./DIMAS_BRILIANT, HACKED BY, hacked by, Hacked, security, ./BRILIANT">
+   <meta name="keywords" content="HACKED BY ./BRILIANT, ./BRILIANT, HACKED BY ./DIMAS_BRILIANT, HACKED BY, BRILIANT, HACKED BY BRILIANT, hacked by ./briliant, HACKED BY ./BRILIANT, BRILIANT, briliant, hacked by ./briliant, hacked by briliant, HACKED BY BRILIANT, briliant29, BRILIANT29, ./DIMAS_BRILIANT, HACKED BY, hacked by, Hacked, security">
+   <meta name="robots" content="index, follow">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <meta name="revisit-after" content="10000000000 days">
+   <meta name="description" content="Indonesia">
+   <meta name="author" content="DIMAS BRILIANT">
+</head>
+
+<body>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Condensed');
+
+    body {
+      font-family: "Ubuntu Condensed";
+      color: #fff;
+      background: #000;
+    }
+
+    b {
+      color: red;
+    }
+  </style>
+  <table width="100%" height="100%">
+    <td align="center">
+      <img src="https://fancodelive.com/briliant.png" width="175px"><br><i>Hacked by <b>./BRILIANT</b> </p>
+        TAK PERLU MENAMPAKAN DIRI UNTUK DI SEGANI,<br>
+
+        TAK PERLU CARI MUKA UNTUK DI KAGUMI,<br>
+
+        CUKUP DIAM DAN LAKUKAN MISI<br>
+        <p>From Code To Xploit | please fix this site sir
+          <br>briliant.403.500<b>@</b>gmail.com
+        </p><b>$</b> Greetz <b>$</b><br>
+    </td>
+    <center><br>
+      <button class="btn btn-primary" onclick="toggleMusic()" id="btn-playback" data-play="false">
+        <i class="fa fa-play"></i>
+        <span id="btn-text">HERE!!!</span>
+      </button>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+      <audio id="audio" src="https://fancodelive.com/briliant.mp3" />
+      <script>
+        const myAudio = document.getElementById("audio");
+        const btn = document.getElementById("btn-playback");
+        const btnIcon = document.querySelector("#btn-playback > i");
+        const btnText = document.getElementById("btn-text");
+        const toggleMusic = () => {
+          const dataPlay = btn.getAttribute('data-play');
+          if (dataPlay === 'false') {
+            btn.setAttribute('data-play', 'true');
+            myAudio.play();
+            btnIcon.classList.remove("fa-play");
+            btnIcon.classList.add("fa-pause");
+            btnText.innerText = "PAUSE";
+          } else {
+            btn.setAttribute('data-play', 'false');
+            myAudio.pause();
+            btnIcon.classList.remove("fa-pause");
+            btnIcon.classList.add("fa-play");
+            btnText.innerText = "PLAY";
+          }
+        };
+
+        myAudio.onpause = () => {
+          btn.setAttribute('data-play', 'false');
+          myAudio.pause();
+          btnIcon.classList.remove("fa-pause");
+          btnIcon.classList.add("fa-play");
+          btnText.innerText = "PLAY";
+        };
+      </script>
+
+
+</body>
+</center>
+
+</html>
